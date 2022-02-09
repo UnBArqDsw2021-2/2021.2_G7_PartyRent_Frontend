f60cffa27cddf08ee7d88981b91ebdb80494b523
Flavio Vieira <flavio.vl@gmail.com>
2022-02-09 01:00:56 -0300

    fix: change config eslint and jest

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,6 @@ module.exports = {
     '<rootDir>/src/**/*.{ts,tsx}'
   ],
   coverageDirectory: 'coverage',
-  testPathIgnorePatterns: [
-    '<rootDir>/node_modules/'
-  ],
   testEnvironment: 'node',
   transform: {
     '.+\\.(ts|tsx)$': 'ts-jest'
