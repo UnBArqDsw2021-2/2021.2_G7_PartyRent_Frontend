f73b9b2aae79bc8e1ac2542027dd08ef421af80e
jbisinotti <victormax2006@gmail.com>
2022-04-14 20:52:59 -0300

    feat: adding methods to add and remove products from cart

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
  
-class ShoppingCart extends React.Component {
+class ShoppingCart extends React.Component<{}, {qtd: number}> {
+    constructor(props: any){
+        super(props);
+        this.state = {
+            qtd: 0
+        };
+
+    }
+
+    add() {
+        this.setState({
+            qtd: this.state.qtd + 1
+        });
+    }
+
+    remove() {
+        this.setState({
+           qtd: this.state.qtd - 1   
+        });
+    }
+    
     render() { 
         return (
             <div>
