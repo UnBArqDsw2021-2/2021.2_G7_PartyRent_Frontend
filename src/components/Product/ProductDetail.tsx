7cd10f94f515c4f30b3baa1585a64da5de53f010
Matheus Clemente <matheus_klementt@hotmail.com>
2022-04-16 00:31:08 -0300

    Fetch product on ProductDetails screen
    Co-Authored-By: Victor <77134807+victor-rayan@users.noreply.github.com>

--- src/components/Product/ProductDetail.tsx
+++ src/components/Product/ProductDetail.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom'; 
+import { useState } from 'react';
 
 function ProductDetail() { 
     let params = useParams();
+    const [state, setState] = useState()
+    useEffect(() =>  {
+        fetch('http://127.0.0.1:8000/product/'+params.id+'/',  {
+            "method": "GET"
+        })
+        .then(response => response.json())
+        .then(response => {
+            setState(response)
+            console.log("Finishing fetching product details")
+        })
+    }, []);
+
     return (
         <div>
             <p>Product details</p>
+            <p>{state?.['name']}</p>
             <p>ID: {params.id}</p>
         </div>
     );
